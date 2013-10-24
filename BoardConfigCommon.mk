92cfd09c5e032277e15552da46c855ba1cc95947
SferaDev <sferarc@gmail.com>
2013-10-24 10:11:32 +0200

    aries-common: TWRP-ify
    Add TWRP flags
    Add twrp.fstab
    
    Change-Id: I7764992267599eab267e2f915e554fdbe7b831f7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -96,11 +96,13 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/s3c-usbgadget/gadget/l
 # Recovery
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/aries-common/recovery/graphics.c
+# Disable for TWRP
+# BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/aries-common/recovery/graphics.c
 BOARD_USES_BML_OVER_MTD := true
 BOARD_CUSTOM_BOOTIMG_MK := device/samsung/aries-common/shbootimg.mk
 TARGET_RECOVERY_FSTAB := device/samsung/aries-common/fstab.aries
 RECOVERY_FSTAB_VERSION := 2
+PRODUCT_COPY_FILES += device/samsung/aries-common/twrp.fstab:recovery/root/etc/twrp.fstab
 
 # Boot Animation
 TARGET_BOOTANIMATION_PRELOAD := true
@@ -133,3 +135,16 @@ BOARD_HARDWARE_CLASS := device/samsung/aries-common/cmhw/
 
 # Include aries specific stuff
 -include device/samsung/aries-common/Android.mk
+
+#TWRP Flags
+DEVICE_RESOLUTION := 480x800
+TW_NO_REBOOT_BOOTLOADER := true
+TW_INTERNAL_STORAGE_PATH := "/sdcard"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "sdcard"
+TW_EXTERNAL_STORAGE_PATH := "/emmc"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "emmc"
+TW_FLASH_FROM_STORAGE := true
+TW_EXCLUDE_SUPERSU := true
+TW_NO_PARTITION_SD_CARD := true
+TW_NO_EXFAT_FUSE := true
+TW_INCLUDE_FB2PNG := true

8f8c9252a57726b15daa8b6aaaf9e5129475f156
Pawit Pornkitprasan <p.pawit@gmail.com>
2013-12-07 15:29:19 +0700

    aries-common: enable zRAM by default
    zRAM is now officially supported in 4.4, so enable it by default
    at 18%
    
    Change-Id: I7957de72fe0832d361c5053a982e65906eff113d

--- device_base.mk
+++ device_base.mk
@@ -166,7 +166,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
        ro.telephony.ril.v3=icccardstatus,datacall,signalstrength,facilitylock \
        mobiledata.interfaces=pdp0,eth0,gprs,ppp0 \
        ro.vold.switchablepair=/mnt/emmc,/mnt/sdcard \
-       ro.bq.gpu_to_cpu_unsupported=1
+       ro.bq.gpu_to_cpu_unsupported=1 \
+       ro.zram.default=18 \
 
 PRODUCT_PACKAGES += \
     libnetcmdiface

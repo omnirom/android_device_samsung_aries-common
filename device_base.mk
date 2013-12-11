5cbda5f2d1a8c486ba3200a042e0da0a0c31377c
kasperhettinga <kasper.hettinga@gmail.com>
2013-12-11 19:23:53 +0100

    Enable FRandom
    Depends on kernel patch to be functional
    
    PS2: rebase
    
    Change-Id: I78086adcc4cdd455dc95617d4fb9cbe2ff182d03

--- device_base.mk
+++ device_base.mk
@@ -215,4 +215,8 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 PRODUCT_COPY_FILES += \
     device/samsung/aries-common/updater.sh:updater.sh
 
+# init.d scripts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/etc/init.d/00random:system/etc/init.d/00random
+
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4329/device-bcm.mk)

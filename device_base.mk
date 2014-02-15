b07fc2afb03a674aa985d2052c43914e320cb8a1
Humberto Borba <humberos@gmail.com>
2014-02-15 19:55:16 -0200

    aries-common: use mtp instead mass storage
    actually mass storage isn't supported on omnirom
    so set mtp as well and get internal|external auto mounts
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I7d7354c12f04d4e65e78f60951a161c4745b9de1

--- device_base.mk
+++ device_base.mk
@@ -204,7 +204,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Set default USB interface and default to internal SD as /sdcard
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mass_storage \
+    persist.sys.usb.config=mtp \
     persist.sys.vold.switchexternal=1
 
 include frameworks/native/build/phone-hdpi-512-dalvik-heap.mk

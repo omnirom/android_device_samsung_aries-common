47e871f8cd2268910167e73a42c80cbb8277dafe
Humberto Borba <humberos@gmail.com>
2014-03-02 15:56:11 -0300

    aries-common: bring back missed copyright
    Change-Id: I50b52bb9ecc84108171d7048c34aadae55cd97e1
    Signed-off-by: Humberto Borba <humberos@gmail.com>

--- DeviceSettings/src/org/omnirom/device/TouchKeyBacklightTimeout.java
+++ DeviceSettings/src/org/omnirom/device/TouchKeyBacklightTimeout.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2013 The OmniROM Project
+ * Copyright (C) The CyanogenMod Project
+ * Copyright (C) The OmniROM Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

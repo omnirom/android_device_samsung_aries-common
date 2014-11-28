885716d71361f39376318e3e1a6ad511189277ee
Humberto Borba <humberos@gmail.com>
2014-11-28 15:39:14 -0200

    aries-common: PVR: add re-implemented pvrsrvinit and glue
    as the proprietary pvrsrvinit is non-PIE, add a re-implementation of it.
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: Id1ca27a63a34751b3de75a31c044b25cbe0f78b8

---
+++ pvrsrvinit/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := pvrsrvinit.c
+LOCAL_LDFLAGS := -L vendor/samsung/aries-common/proprietary
+LOCAL_LDLIBS := -lsrv_init -lsrv_um
+LOCAL_MODULE_PATH := $(TARGET_OUT)/bin/
+LOCAL_MODULE := pvrsrvinit
+LOCAL_MODULE_TAGS := optional
+LOCAL_SHARED_LIBRARIES:= libc libdl libcutils
+
+include $(BUILD_EXECUTABLE)

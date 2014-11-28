d5e8e1b366e07ad3c6ad4c8c3a6267c02c205e0c
Humberto Borba <humberos@gmail.com>
2014-11-28 15:40:45 -0200

    aries-common: bring up android 5.0
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I8f89a98cf7950d4324c67c593d043ac3e4a3c3eb

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -111,8 +111,8 @@ BOARD_CHARGER_DIM_SCREEN_BRIGHTNESS := true
 BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/aries-common/recovery/graphics.c
 
 # Boot Animation
+BOARD_NEEDS_LOWFPS_BOOTANI := true
 TARGET_BOOTANIMATION_PRELOAD := true
-TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 TARGET_BOOTANIMATION_USE_RGB565 := true
 
 # SkTextBox for libtvout
@@ -142,19 +142,12 @@ COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
 BOARD_SCREENRECORD_LANDSCAPE_ONLY := true
 
 # SELinux
-#BOARD_SEPOLICY_DIRS += \
-#    device/samsung/aries-common/sepolicy
+BOARD_SEPOLICY_DIRS += \
+    device/samsung/aries-common/sepolicy
 
-#BOARD_SEPOLICY_UNION += \
-#    bdaddr_read.te \
-#    file_contexts \
-#    geomagneticd.te \
-#    orientationd.te \
-#    property_contexts \
-#    pvrsrvinit.te \
-#    rild.te \
-#    tvouthack.te \
-#    tvoutserver.te \
+BOARD_SEPOLICY_REPLACE += \
+    domain.te \
+    app.te
 
 # Hardware tunables
 # BOARD_HARDWARE_CLASS := device/samsung/aries-common/cmhw/

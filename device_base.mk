61be39028d443b6fce2a045c69dee892a5463c68
Humberto Borba <humberos@gmail.com>
2014-11-28 15:31:30 -0200

    aries-common: add init.trace.rc file
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I92cc61e21f69b2bf32c83df90f290c94dc198959

--- device_base.mk
+++ device_base.mk
@@ -48,6 +48,7 @@ PRODUCT_COPY_FILES := \
 # Init files
 PRODUCT_COPY_FILES += \
     device/samsung/aries-common/init.aries.rc:root/init.aries.rc \
+    device/samsung/aries-common/init.trace.rc:root/init.trace.rc \
     device/samsung/aries-common/init.aries.gps.rc:root/init.aries.gps.rc \
     device/samsung/aries-common/init.aries.usb.rc:root/init.aries.usb.rc \
     device/samsung/aries-common/init.recovery.aries.rc:root/init.recovery.aries.rc \

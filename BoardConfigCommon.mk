efa0f633f0512099687dffa5211e0f2376f412d9
Humberto Borba <humberos@gmail.com>
2014-12-11 20:23:35 -0200

    aries-common: fix internal sdcard mount
    requires:
    https://gerrit.omnirom.org/11036
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I19ab4ed9a6850730f94e0b603aed4cb665f7a2b9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -93,6 +93,7 @@ WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 BOARD_BLUEDROID_VENDOR_CONF := device/samsung/aries-common/libbt_vndcfg.txt
 
 # Vold
+BOARD_VOLD_USES_SEQUENCIAL_PARTITIONS := true
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/s3c-usbgadget/gadget/lun%d/file"
 

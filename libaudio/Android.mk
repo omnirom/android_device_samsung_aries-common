fc2708e7261d94376c9853a603b63dd13dd47601
rooque <victor.rooque@gmail.com>
2014-12-11 21:34:16 -0200

    aries-common: add flag to libaudio
    -- > USE_OLD_LIBAUDIO
    
    Change-Id: Icd9d7f1a4124779315b6b24b6689f7ae921ef2bf

--- libaudio/Android.mk
+++ libaudio/Android.mk
@@ -25,6 +25,8 @@ LOCAL_C_INCLUDES += \
 	$(call include-path-for, audio-effects) \
 	$(call include-path-for, audio-utils)
 
+LOCAL_CFLAGS += -DUSE_OLD_LIBAUDIO
+
 ifeq ($(BOARD_USES_FROYO_RILCLIENT),true)
   LOCAL_CFLAGS += -DUSES_FROYO_RILCLIENT
 endif

1901e3bdff8297ebb52451d25582bd46f6b9a63b
Humberto Borba <humberos@gmail.com>
2015-01-30 00:03:03 -0200

    aries-common: board: remove unsupported audio config
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I42b270a7a4935a797d94fb748b27f3c982b8187b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -19,6 +19,8 @@
 
 # Set this up here so that BoardVendorConfig.mk can override it
 BOARD_USES_GENERIC_AUDIO := false
+AUDIO_FEATURE_ENABLED_INCALL_MUSIC := false
+AUDIO_FEATURE_ENABLED_COMPRESS_VOIP := false
 
 BOARD_USES_LIBSECRIL_STUB := true
 

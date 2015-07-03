6f5a5b117a10b72eda739e6822fd26a7c7c3e6b7
Humberto Borba <humberos@gmail.com>
2015-07-03 15:49:33 -0300

    aries-common: Update toolchain path
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: If3a2b19b2e8704c394284ddd49df4d3db9c06e90

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -31,7 +31,7 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_ARCH_VARIANT_CPU := cortex-a8
 TARGET_CPU_VARIANT := cortex-a8
 TARGET_CPU_SMP := false
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7
+KERNEL_TOOLCHAIN := "$(ANDROID_BUILD_TOP)/prebuilts/gcc/$(strip $(HOST_OS))-x86/arm/arm-eabi-4.7/bin/"
 
 # Bionic stuff
 BOARD_USES_LEGACY_MMAP := true

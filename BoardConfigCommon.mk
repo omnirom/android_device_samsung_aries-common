627a9127e3f1951db4c51044567d2b9166d2b358
Humberto Borba <humberos@gmail.com>
2015-07-04 00:03:57 -0300

    aries-common: Add BOARD_HAS_NON_EMULATED_SDCARD
    depends on:
    
        https://gerrit.omnirom.org/#/c/13349/
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: Ica0223a499e1b1b35ff04d1465b1a5d291a9cbf1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -109,6 +109,7 @@ BOARD_BLUEDROID_VENDOR_CONF := device/samsung/aries-common/libbt_vndcfg.txt
 # Vold
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/s3c-usbgadget/gadget/lun%d/file"
+BOARD_HAS_NON_EMULATED_SDCARD := true
 
 # Enable dex-preoptimization to speed up first boot sequence
 ifeq ($(HOST_OS),linux)
